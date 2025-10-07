3821c65f804edde2106aef8ca7ce156f9734b3d3
gandharvj937599-ui <gandharvj937599@gmail.com>
2025-10-07 21:29:55 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
 
+export default function Page() {
+  return (
+    <main style={{ textAlign: 'center', marginTop: '20%' }}>
+      <h1>🚀 Stock Market Prediction App</h1>
+      <p>Frontend deployed successfully on Vercel!</p>
+    </main>
+  );
+}
